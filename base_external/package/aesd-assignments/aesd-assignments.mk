151b06d25490c36fc9df8df1f6aa77ff7e6f5a98
tuyenvo <thanhtuyenngt@gmail.com>
2024-04-30 14:37:43 -0700

    Improve code after add aesdsocket-start-stop to repos assignment 3 and later

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5cc3681e9e8f3af96bf10f4c13f0a58b7c6405fa'
+AESD_ASSIGNMENTS_VERSION = '57eeb3cac95c371f871ff1c5eb9c19ca69250009'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
